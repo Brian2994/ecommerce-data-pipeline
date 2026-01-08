879eff6315ab79d6d3769f1ed027cff03169ae1c
Brian2994 <brianpari29@gmail.com>
2026-01-08 20:38:46 -0300

    Arquivo modificado

--- src/load/load_bigquery.py
+++ src/load/load_bigquery.py
@@ -6,3 +6,29 @@ DATASET = "ecommerce_trusted"
 TRUSTED_PATH = Path("data/trusted")
 
 client = bigquery.Client(project=PROJECT_ID)
+
+def load_parquet(table_name: str, file_path: Path):
+    table_id = f"{PROJECT_ID}.{DATASET}.{table_name}"
+
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.PARQUET,
+        write_disposition="WRITE_TRUNCATE" # WRITE_APPEND
+    )
+
+    with open(file_path, "rb") as f:
+        job = client.load_table_from_file(
+            f,
+            table_id,
+            job_config=job_config
+        )
+
+    job.result()
+    print(f"Tabela {table_name} carregada com sucesso!")
+
+def main():
+    load_parquet("products", TRUSTED_PATH / "products.parquet")
+    load_parquet("users", TRUSTED_PATH / "users.parquet")
+    load_parquet("orders_items", TRUSTED_PATH / "orders_items.parquet")
+
+if __name__ == "__main__":
+    main()
